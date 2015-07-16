a59a34e796a3ff597c993f132a5e3c8c79d2f61f
Andrey Mussatov <andr435@gmail.com>
2015-07-16 18:57:46 +0300

    stage 4b

---
+++ app/scripts/controllers/auth.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Andrey on 7/16/15.
+ */
+'use strict';
+
+app.controller('AuthCtrl', function($scope, $location, Auth, user){
+    if (user){
+        $location.path('/');
+    }
+
+    $scope.register = function(){
+        Auth.register($scope.user).then(function(){
+           return Auth.login($scope.user).then(function(){
+               $location.path('/');
+           });
+        });
+    };
+});
